ebd7a6bd9620977387b0d68c0774d1029bf4d9ab
hoh miyazawa <hohMiyazawa@yandex.com>
2021-01-16 18:02:13 +0100

    more header parsing

--- dhoh.cpp
+++ dhoh.cpp
@@ -35,8 +35,15 @@ void print_usage(){
 	7: terminated before completion
 	255: unknown error
 */
-int read_varint(uint8_t* bytes, int* location){
-	return bytes[*location++];//incorrect implentation! only works up to 127
+int read_varint(uint8_t* bytes, int* location){//incorrect implentation! only works up to 16383!
+	uint8_t first_byte = bytes[*location++];
+	if(first_byte & (1<<7)){
+		uint8_t second_byte = bytes[*location++];
+		return (((int)first_byte & 0b01111111) << 7) + second_byte;
+	}
+	else{
+		return (int)first_byte;
+	}
 }
 
 int main(int argc, char *argv[]){
@@ -97,7 +104,7 @@ int main(int argc, char *argv[]){
 	}
 	uint8_t bit_depth = in_bytes[byte_pointer++];
 	if(pixel_format == 0 && !(bit_depth == 0 || bit_depth == 1)){
-		printf("invalid bitstream!\n");
+		printf("bit depth is too high for a bit image!\n");
 		return 4;
 	}
 	printf("bit depth: %d\n",(int)bit_depth);
@@ -106,9 +113,61 @@ int main(int argc, char *argv[]){
 	printf("dimensions: %dx%d\n",width,height);
 	if(bit_depth == 0){
 		if(byte_pointer < in_size){
-			printf("invalid bitstream!\n");
+			printf("file is too large for a blank image!\n");
 			return 4;
 		}
+		else{
+			//write blank image
+			return 5;
+		}
+	}
+	uint8_t x_tiles = in_bytes[byte_pointer++] + 1;
+	uint8_t y_tiles = in_bytes[byte_pointer++] + 1;
+	if(x_tiles != 1 || y_tiles != 1){
+		printf("tiled image: %dx%d\n",(int)x_tiles,(int)y_tiles);
+		printf("tile offset 0: 0\n");
+		for(int i=1;i<x_tiles*y_tiles;i++){
+			printf("tile offset %d: %d\n",i,read_varint(in_bytes, &byte_pointer));
+		}
+	}
+	else{
+		printf("single tile image\n");
+	}
+	uint8_t pixel_format_internal = in_bytes[byte_pointer++];
+	if(pixel_format == 0){
+		printf("pixel format: bit\n");
+	}
+	else if(pixel_format_internal == 1){
+		printf("pixel format: greyscale\n");
+	}
+	else if(pixel_format_internal == 2){
+		printf("pixel format: rgb\n");
+	}
+	else if(pixel_format_internal == 3){
+		printf("pixel format: greyscale + alpha\n");
+	}
+	else if(pixel_format_internal == 4){
+		printf("pixel format: rgb + alpha\n");
+	}
+	else if(pixel_format_internal == 128){
+		printf("pixel format: subgreen\n");
+	}
+	else if(pixel_format_internal == 129){
+		printf("pixel format: YIQ\n");
+	}
+	else if(pixel_format_internal == 130){
+		printf("pixel format: subgreen + alpha\n");
+	}
+	else if(pixel_format_internal == 131){
+		printf("pixel format: YIQ + alpha\n");
+	}
+	else if(pixel_format_internal == 255){
+		printf("invalid pixel format!\n");
+		return 4;
+	}
+	else{
+		printf("unknown pixel format!\n");
+		return 5;
 	}
 
 	delete[] in_bytes;
