ecbfea38ac4fc6781cb2c141bba3d50eae2f4492
hoh miyazawa <hohMiyazawa@yandex.com>
2021-01-16 17:36:47 +0100

    specification amended

--- dhoh.cpp
+++ dhoh.cpp
@@ -23,6 +23,18 @@ void print_usage(){
 	printf("You can turn that into an image with imagemagick:\n");
 	printf("[TODO: imagemagick command]\n");
 }
+/*
+	return values:
+	0: OK
+	1: CLI error
+	2: I/O error
+	3: unexpected end of file
+	4: incorrect bitstream
+	5: not implemented
+	6: resources exhausted
+	7: terminated before completion
+	255: unknown error
+*/
 
 int main(int argc, char *argv[]){
 	printf("Not ready for actual usage!\n");
@@ -35,9 +47,53 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 	else if(argc < 3){
-		printf("not enough arguments\n");
+		printf("not enough arguments!\n");
 		print_usage();
 		return 1;
 	}
+
+	size_t in_size;
+	uint8_t* in_bytes = read_file(argv[1], &in_size);
+
+	if(in_size < 8){
+		printf("not a valid hoh file!\n");
+		return 3;
+	}
+	int byte_pointer = 0;
+	if(
+		in_bytes[byte_pointer++] != 153
+		|| in_bytes[byte_pointer++] != 72
+		|| in_bytes[byte_pointer++] != 79
+		|| in_bytes[byte_pointer++] != 72
+	){
+		printf("not a valid hoh file!\n");
+		return 4;
+	}
+	uint8_t pixel_format = in_bytes[byte_pointer++];
+	if(pixel_format == 0){
+		printf("pixel format: bit\n");
+	}
+	else if(pixel_format == 1){
+		printf("pixel format: greyscale\n");
+	}
+	else if(pixel_format == 2){
+		printf("pixel format: rgb\n");
+	}
+	else if(pixel_format == 3){
+		printf("pixel format: greyscale + alpha\n");
+	}
+	else if(pixel_format == 4){
+		printf("pixel format: rgb + alpha\n");
+	}
+	else if(pixel_format == 255){
+		printf("invalid pixel format!\n");
+		return 4;
+	}
+	else{
+		printf("unknown pixel format!\n");
+		return 5;
+	}
+
+	delete[] in_bytes;
 	return 0;
 }
